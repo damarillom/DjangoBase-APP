b7dcc67cee26fa774d9b733a4be019f5c1126d97
Daniel Amarillo Morales <32068902+damarillom@users.noreply.github.com>
2023-12-20 22:28:17 +0100

    working on word finder

--- DjangoBase/app/tests.py
+++ DjangoBase/app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from app.word_finder import WordFinder
 
-# Create your tests here.
+class WordFinderTestCase(TestCase):
+    def test_longest_word(self):
+        word_list = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune']
+        word_finder = WordFinder(word_list)
+        result = word_finder.longest_word('ajsxuytcnhre')
+        self.assertEqual(result, 'saturn')
+
+
